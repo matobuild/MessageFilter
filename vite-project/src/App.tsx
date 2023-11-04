03eab95199b2b81b625ba485c9346b9bc607d693
Mato <Kittawat@icloud.com>
2023-11-04 13:21:23 +0800

    first commit

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import { useState } from "react";
+
+//mock data
+import userData from "./userData";
+
+function App() {
+  const [showData, setShowData] = useState(userData);
+
+  return (
+    <div className="container">
+      <ul>
+        {showData.map((user) => {
+          return (
+            <>
+              <li key={user.id}>{`${user.first_name} ${user.last_name}`}</li>
+              <li key={user.email}></li>
+            </>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
