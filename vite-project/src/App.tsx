b405718333462f532b44cdc786d6750aa46d5c91
Mato <Kittawat@icloud.com>
2023-11-04 14:48:27 +0800

    import img successfukllly

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,22 +3,34 @@ import { useState } from "react";
 
 //mock data
 import userData from "./userData";
+// import icon
+import icon from "./assets/icon.png";
 
 function App() {
   const [showData, setShowData] = useState(userData);
 
   return (
     <div className="container">
-      <ul>
-        {showData.map((user) => {
-          return (
-            <>
-              <li key={user.id}>{`${user.first_name} ${user.last_name}`}</li>
-              <li key={user.email}>{user.email}</li>
-            </>
-          );
-        })}
-      </ul>
+      {showData.map((user) => {
+        return (
+          <div className="flex" key={user.id}>
+            <div>
+              <img
+                className="rounded-full"
+                src={user.avatar}
+                alt="no-img-found"
+              />
+            </div>
+            <ul>
+              <li>{`${user.first_name} ${user.last_name}`}</li>
+              <li>{user.email}</li>
+            </ul>
+            <div>
+              <img src={icon} alt="no-icon-found" />
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
