96618f3cf5a935ab35a943738e5c14add414e5df
Mato <Kittawat@icloud.com>
2023-11-04 19:56:45 +0800

    4

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -13,23 +13,26 @@ function App() {
     <div className="container p-5">
       {showData.map((user) => {
         return (
-          <div className="flex p-3" key={user.id}>
-            <div className="basis-1/4">
-              <img
-                className="rounded-full "
-                src={user.avatar}
-                alt="no-img-found"
-              />
+          <div className="mx-3 mt-3">
+            <div className="flex" key={user.id}>
+              <div className="basis-1/4">
+                <img
+                  className="rounded-full "
+                  src={user.avatar}
+                  alt="no-img-found"
+                />
+              </div>
+              <div className="basis-2/4">
+                <ul>
+                  <li>{`${user.first_name} ${user.last_name}`}</li>
+                  <li className="text-gray-400">{user.email}</li>
+                </ul>
+              </div>
+              <div className="basis-1/4">
+                <img className="w-1/12" src={icon} alt="no-icon-found" />
+              </div>
             </div>
-            <div className="basis-2/4">
-              <ul>
-                <li>{`${user.first_name} ${user.last_name}`}</li>
-                <li className="text-gray-400">{user.email}</li>
-              </ul>
-            </div>
-            <div className="basis-1/4">
-              <img className="w-1/12" src={icon} alt="no-icon-found" />
-            </div>
+            <hr className="mt-3" />
           </div>
         );
       })}
