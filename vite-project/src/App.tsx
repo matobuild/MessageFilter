26077824b9700158277a1626e3c52009e36ce771
Mato <Kittawat@icloud.com>
2023-11-05 22:26:33 +0800

    latest update

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,21 +1,15 @@
 import "./App.css";
 import React, { useState } from "react";
-
-//mock data
 import userData from "./userData";
-// import icon
 import icon from "./assets/icon.png";
 
 function DataTable() {
   const [showData, setShowData] = useState(userData);
-
-  // todo: display the data`
-
   return (
     <div className="container p-5">
       {showData.map((user) => {
         return (
-          <div className="mx-3 mt-3">
+          <div className="mx-3 mt-3" key={user.id}>
             <div className="flex" key={user.id}>
               <div className="basis-1/4">
                 <img
@@ -24,14 +18,14 @@ function DataTable() {
                   alt="no-img-found"
                 />
               </div>
-              <div className="basis-2/4">
+              <div className="basis-2/4 flex place-items-center ml-3     ">
                 <ul>
                   <li>{`${user.first_name} ${user.last_name}`}</li>
                   <li className="text-gray-400">{user.email}</li>
                 </ul>
               </div>
-              <div className="basis-1/4">
-                <img className="w-1/12" src={icon} alt="no-icon-found" />
+              <div className="basis-1/4 grid place-content-center place-items-center  ">
+                <img className="w-1/3" src={icon} alt="no-icon-found" />
               </div>
             </div>
             <hr className="mt-3" />
@@ -49,6 +43,7 @@ function Search() {
     setValue(event.target.value);
     const textFind = event.target.value;
     if (textFind.length > 2) {
+      // todo:get input value
       // todo: search the input data
       // todo: need to input the DATA so can use the value show DATA, how to pass data betwee function
       // const person  = showData.filter(
